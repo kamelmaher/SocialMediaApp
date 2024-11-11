c9cbc892add14e1a2ab2097d926d726e3eb3f9a9
kamelmaher <kamelmahersh@gmail.com>
2024-11-11 14:29:37 +0200

    Edit Post Card

--- src/components/HomePage/CreatePost/CreatePost.tsx
+++ src/components/HomePage/CreatePost/CreatePost.tsx
@@ -1,13 +1,18 @@
-import logo from "../../../../img/icons/facebook.png"
+import {  faImages, faVideo } from "@fortawesome/free-solid-svg-icons"
+import { faFaceLaugh } from "@fortawesome/free-regular-svg-icons"
+import person from "../../../../img/picture.jpg"
+import "./post.css"
 import PostType from "./PostType"
 
 const CreatePost = () => {
+    const icons = [faVideo , faImages , faFaceLaugh]
     const types = ["Live Video", "Photo/Image", "Feeling/Activity"]
+    const colors = ["#f02849", "#45bd62", "#f7b928"]
     return (
-        <div className="p-2 my-card">
+        <div className="my-card">
             <div className="d-flex gap-2">
                 <div className="img">
-                    <img src={logo} alt="" className="img-fluid post-img" />
+                    <img src={person} alt="" className="img-fluid post-img rounded-circle" />
                 </div>
                 <div className="post-text">
                     <input type="text" placeholder="Whats on your mind, Kamel?" className="post-input form-control" />
@@ -16,8 +21,8 @@ const CreatePost = () => {
             <hr className="mt-2 mb-1" />
             <div className="row">
                 {
-                    types.map(e => {
-                        return <PostType key={e} postType={e} />
+                    types.map((e , i) => {
+                        return <PostType key={e} postType={e} icon={icons[i]} color={colors[i]}/>
                     })
                 }
             </div>
--- src/components/HomePage/CreatePost/PostType.tsx
+++ src/components/HomePage/CreatePost/PostType.tsx
@@ -1,16 +1,21 @@
-import { faHouse } from "@fortawesome/free-solid-svg-icons"
+import { IconProp } from "@fortawesome/fontawesome-svg-core"
+// import { faHouse } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 type PostTypeProps = {
-    postType: string
+    postType: string,
+    icon: IconProp
+    color: string
 }
-const PostType = ({ postType }: PostTypeProps) => {
+const PostType = ({ postType , icon , color}: PostTypeProps) => {
     return (
-        <div className="col-4 pointer">
+        <div className="col-4 pointer post-type">
             <div className="p-2 d-flex gap-1 justify-content-center align-items-center">
                 <div className="post-icon">
-                    <FontAwesomeIcon icon={faHouse} className="pt-0" />
+                    <FontAwesomeIcon icon={icon} className="pt-0" color={color}/>
                 </div>
-                <h6 className="mb-0">{postType}</h6>
+                <h6 className="mb-0" style={{
+                    color: "#65686c"
+                }}>{postType}</h6>
             </div>
         </div>
     )
